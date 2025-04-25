6130c1b55bb4a4ea5a2faca1ddb4486f00a0d154
corb0140 <corb0140@algonquinlive.com>
2025-04-25 19:35:02 -0400

    added login authentication

--- server/src/controllers/authControllers.js
+++ server/src/controllers/authControllers.js
@@ -1,4 +1,4 @@
-const bycrypt = require("bcrypt");
+const bcrypt = require("bcrypt");
 const pool = require("../config/db");
 const logger = require("../helpers/logger");
 const {
@@ -9,12 +9,8 @@ const {
 const register = async (req, res) => {
   const { email, password } = req.body;
 
-  if (typeof password !== "string") {
-    return res.status(400).json({ error: "Password must be a string" });
-  }
-
   try {
-    // CHeck if user already exists
+    // Check if user already exists
     const { rows } = await pool.query(
       "SELECT EXISTS (SELECT 1 FROM users WHERE email = $1)",
       [email]
@@ -24,7 +20,7 @@ const register = async (req, res) => {
       return res.status(400).json({ message: "User already exists" });
 
     // Hash password
-    const hashedPassword = await bycrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     // Insert user into DB
     const newUser = await pool.query(
@@ -52,6 +48,10 @@ const register = async (req, res) => {
     });
 
     // Send access token
+    // 201 means request was successful and new resource was created
+    // .json converts the response to JSON format
+    // sets the Content-Type header to application/json
+    // and sends the JSON response to the client
     res.status(201).json({
       message: "User registered successfully",
       accessToken,
@@ -62,6 +62,57 @@ const register = async (req, res) => {
   }
 };
 
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Find user by email
+    const { rows } = await pool.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+
+    const user = rows[0];
+
+    // check if email exists
+    if (!user.email) {
+      return res.status(400).json({ message: "Invalid email" });
+    }
+
+    // Compare passwords
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword)
+      return res.status(400).json({ message: "Invalid password" });
+
+    // Check if user exists
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    // Generate tokens
+    const accessToken = generateAccessToken(user);
+    const refreshToken = generateRefreshToken(user);
+
+    // Set the refresh token in a cookie
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    // 200 means request was success
+    res.status(200).json({
+      message: "User logged in successfully",
+      accessToken,
+    });
+  } catch (error) {
+    logger.error("Error in login controller", error);
+    // 500 means server error
+    return res.status(500).json({ message: "server error" });
+  }
+};
+
 module.exports = {
   register,
+  login,
 };
--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -3,7 +3,7 @@ const authRouter = Router();
 const authController = require("../controllers/authControllers");
 
 authRouter.post("/register", authController.register);
-// authRouter.post("/login", authController.login);
+authRouter.post("/login", authController.login);
 // authRouter.post("/logout", authController.logout);
 // authRouter.post("forgot-password", authController.forgotPassword);
 // authRouter.post("/reset-password", authController.resetPassword);

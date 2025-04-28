60ce04557dc172e6c75a0ae9e6a785e39aa0e78a
corb0140 <corb0140@algonquinlive.com>
2025-04-28 19:50:31 -0400

    axios setup

---
+++ client/src/api/axios.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+/**
+ * @description This file contains the axios instance used for making API requests.
+ * It is configured with a base URL and credentials.
+ */
+const instance = axios.create({
+  baseURL: "http://localhost:8000/auth", // Base URL for the backend API
+  withCredentials: true, // send cookies with requests
+});
+
+export default instance;
